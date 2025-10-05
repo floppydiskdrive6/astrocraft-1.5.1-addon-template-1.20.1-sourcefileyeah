70d7dd19f36d550bcbd78df707c5f7298f4811ba
Child2 <floppydiskdrive6@gmail.com>
2025-10-04 18:36:26 -0600

    very cool

---
+++ src/client/java/net/witherstorm8475/astrocraftaddon/AstroCraft151AddonDataGenerator.java
@@ -0,0 +1,11 @@
+package net.witherstorm8475.astrocraftaddon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AstroCraft151AddonDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
