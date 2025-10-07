923a80d291f4907d0c0d40d20c0cee476861ed6c
Child2 <floppydiskdrive6@gmail.com>
2025-10-07 00:16:12 -0600

    very cool

--- src/client/java/net/witherstorm8475/astrocraftaddon/mixin/client/OrbitAccessor.java
+++ src/client/java/net/witherstorm8475/astrocraftaddon/mixin/client/OrbitAccessor.java
@@ -1,4 +1,11 @@
 package net.witherstorm8475.astrocraftaddon.mixin.client;
 
-public class OrbitAccessor {
+import mod.lwhrvw.astrocraft.planets.position.Orbit;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(Orbit.class)
+public interface OrbitAccessor {
+    @Invoker("astrocraftAddon$setPrecession")
+    void invokeSetPrecession(double nodalPeriod, double apsidalPeriod);
 }
