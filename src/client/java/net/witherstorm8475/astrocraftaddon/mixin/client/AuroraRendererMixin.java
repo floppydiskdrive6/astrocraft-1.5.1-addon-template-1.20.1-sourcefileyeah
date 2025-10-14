ba7415d8302559b4aa9bed0e8fa029d55c762a96
Child2 <floppydiskdrive6@gmail.com>
2025-10-13 22:34:25 -0600

    very cool

--- src/client/java/net/witherstorm8475/astrocraftaddon/mixin/client/AuroraRendererMixin.java
+++ src/client/java/net/witherstorm8475/astrocraftaddon/mixin/client/AuroraRendererMixin.java
@@ -2,21 +2,33 @@ package net.witherstorm8475.astrocraftaddon.mixin.client;
 
 import mod.lwhrvw.astrocraft.SkyRenderer;
 import net.witherstorm8475.astrocraftaddon.atmospheric.AuroraRenderer;
-import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.lang.reflect.Method;
+
 @Mixin(value = SkyRenderer.class, remap = false)
 public class AuroraRendererMixin {
 
     @Inject(method = "renderMain", at = @At("TAIL"), remap = false)
-    private static void renderAuroras(Matrix4f viewMatrix, Matrix4f projMatrix, float tickDelta, CallbackInfo ci) {
+    private static void renderAuroras(CallbackInfo ci) {
         try {
-            AuroraRenderer.render(viewMatrix, projMatrix, tickDelta);
+            // reflect AuroraRenderer.render(Matrix4f, Matrix4f, float)
+            Class<?> auroraClass = Class.forName("net.witherstorm8475.astrocraftaddon.atmospheric.AuroraRenderer");
+            Method renderMethod = auroraClass.getDeclaredMethod("render",
+                    Class.forName("org.joml.Matrix4f"),
+                    Class.forName("org.joml.Matrix4f"),
+                    float.class
+            );
+            renderMethod.setAccessible(true);
+
+            // If you don't have these matrices, pass null and tickDelta=0
+            renderMethod.invoke(null, null, null, 0.0f);
+
         } catch (Exception e) {
-            // Avoid crashing or spamming logs if rendering fails
+            e.printStackTrace();
         }
     }
 }
